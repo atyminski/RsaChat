0ee245436e6cfa49a313d38fdc457ed48e1ba115
gevlee <gevleeog@gmail.com>
2016-11-05 19:29:57 +0100

    initialize server console

---
+++ Gevlee.RsaChat.Server.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Gevlee.RsaChat.Server.Console
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			System.Console.WriteLine("It works!");
+			System.Console.ReadKey();
+		}
+	}
+}
